bb66dd3021908aaaf2a9d000a7184393846a62c6
Arif Cagirici <acagirici23@gmail.com>
2021-01-08 04:20:13 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,7 +11,13 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do 
-    
+     @pet = Pet.create(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:owner][:name])
+    else
+      @pet.owner = Owner.find_by_id(params[:pet][:owner_id])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
